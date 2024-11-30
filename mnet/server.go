bb9f205959db68cdd588de1ae30e61d96f80a4ce
meowrain <meowrain@126.com>
2024-11-30 13:13:45 +0800

    feat: 编写基本tcp连接代码

--- mnet/server.go
+++ mnet/server.go
@@ -1,6 +1,11 @@
 package mnet
 
-import "mosquito/miface"
+import (
+	"fmt"
+	"mosquito/miface"
+	. "mosquito/mlogger"
+	"net"
+)
 
 // Server IServer的接口实现
 type Server struct {
@@ -11,7 +16,40 @@ type Server struct {
 }
 
 func (server *Server) Start() {
-	//TODO implement me
+	MLogger.Info(fmt.Sprintf("Starting server %s on %s:%d", server.Name, server.IP, server.Port))
+	//获取TCP ADDR
+	addr, err := net.ResolveTCPAddr(server.IPVersion, fmt.Sprintf("%s:%d", server.IP, server.Port))
+	if err != nil {
+		MLogger.Error(fmt.Sprintf("resolve tcp addr failed: %v", err))
+		return
+	}
+	//监听服务器地址
+	listener, err := net.ListenTCP(server.IPVersion, addr)
+	if err != nil {
+		MLogger.Error(fmt.Sprintf("listen tcp failed: %v", err))
+		return
+	}
+	MLogger.Info(fmt.Sprintf("%s Listening on %s:%d", server.Name, server.IP, server.Port))
+	//阻塞等待客户端连接,处理相关请求
+	for {
+		conn, err := listener.Accept()
+		defer conn.Close()
+		if err != nil {
+			MLogger.Error(fmt.Sprintf("accept failed: %v", err))
+			continue
+		}
+		MLogger.Info(fmt.Sprintf("Accepted connection from %s", conn.RemoteAddr()))
+		go func() {
+			buf := make([]byte, 1024)
+			cnt, err := conn.Read(buf)
+			if err != nil {
+				MLogger.Error(fmt.Sprintf("receive from client failed: %v", err))
+			}
+			if _, err := conn.Write(buf[:cnt]); err != nil {
+				MLogger.Error(fmt.Sprintf("send to client failed: %v", err))
+			}
+		}()
+	}
 
 }
 
@@ -21,7 +59,7 @@ func (server *Server) Stop() {
 }
 
 func (server *Server) Serve() {
-	//TODO implement me
+	server.Start()
 
 }
 
--- mserver/server.go
+++ mserver/server.go
@@ -1,8 +1,12 @@
 package main
 
 import "mosquito/mnet"
+import "mosquito/mlogger"
 
 func main() {
+	//初始化日志库
+	mlogger.InitLogger()
+
 	server := mnet.NewServer("Mosquito")
 	server.Serve()
 }
